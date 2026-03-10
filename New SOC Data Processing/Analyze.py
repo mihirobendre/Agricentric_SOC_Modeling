ed74ee994c71871a7c20ab54a73272b96759cbd0
Mihir Bendre <mihirobendre@utexas.edu>
2026-03-11 00:57:09 +0300

    Updated approach

--- New SOC Data Processing/Analyze.py
+++ New SOC Data Processing/Analyze.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 df = pd.read_csv('Kenya_SOC_Data_w_FAO_Soil_Type.csv')
 
+###############################
+# RESULTS BY COOP & TYPE ######
+###############################
+
 # Average BD using rows where BD present
 bd_avg = (
     df.dropna(subset=["BD (g/cm3)"])
@@ -28,8 +32,43 @@ grouped["SOC (t/ha)"] = grouped["avg_BD"] * grouped["avg_C"] * 30
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-print("Results: ")
+print("Results by Coop & Soil Type: ")
 print(grouped)
+
+
+###############################
+# RESULTS BY SOIL TYPE ONLY ###
+###############################
+
+# Average BD using rows where BD present
+bd_avg = (
+    df.dropna(subset=["BD (g/cm3)"])
+      .groupby(["FAO Soil Type"], as_index=False)["BD (g/cm3)"]
+      .mean()
+      .rename(columns={"BD (g/cm3)":"avg_BD"})
+)
+
+# Average C using rows where C present
+c_avg = ( 
+    df.dropna(subset=["C (%)"])
+      .groupby(["FAO Soil Type"], as_index=False)["C (%)"]
+      .mean()
+      .rename(columns={"C (%)":"avg_C"})
+)
+
+# Merge averages
+grouped = pd.merge(bd_avg, c_avg, on=["FAO Soil Type"], how="outer")
+
+# Compute SOC where both exist
+grouped["SOC (t/ha)"] = grouped["avg_BD"] * grouped["avg_C"] * 30
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+print("Results by Soil Type only: ")
+print(grouped)
+
+
 
 
 # Making sure all soil types present are analyzed for SOC and BD:
